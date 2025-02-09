adae4b42899c96684bf3442ac327173e3101ff4a
ivleteuff <leteuffivan@outlook.fr>
2025-02-09 15:23:13 +0100

    Adding architecture

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const carousel = document.querySelector(".carousel");
+    let scrollAmount = 0;
+    
+    setInterval(() => {
+        scrollAmount += 200;
+        if (scrollAmount >= carousel.scrollWidth) {
+            scrollAmount = 0;
+        }
+        carousel.scrollTo({ left: scrollAmount, behavior: "smooth" });
+    }, 3000);
+});
